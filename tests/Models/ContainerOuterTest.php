6de0b6e54426e53a6436d3acb246df84a73abb38
psrearick <psrearick@users.noreply.github.com>
2022-05-12 05:41:06 +0000

    Fix styling

--- tests/Models/ContainerOuterTest.php
+++ tests/Models/ContainerOuterTest.php
@@ -4,7 +4,7 @@ use Psrearick\Containers\Tests\ImplementationClasses\Container;
 use Psrearick\Containers\Tests\ImplementationClasses\Outer;
 
 test('an outer can receive a container', function () {
-    $outer = Outer::factory()->create();
+    $outer     = Outer::factory()->create();
     $container = Container::factory()->create();
 
     $outer->receiveItem($container);
